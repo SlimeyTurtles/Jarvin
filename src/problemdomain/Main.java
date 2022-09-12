7eca135ce89cbd0250a7eefe2323ae4360083d0c
SlimeyTurtles <avinhahuynh@gmail.com>
2022-09-12 01:58:33 -0700

    fas

---
+++ src/problemdomain/Main.java
@@ -0,0 +1,23 @@
+package problemdomain;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        ImageIcon image = new ImageIcon("./frogs/frog.jpg");
+
+        JLabel label = new JLabel();
+        label.setText("text");
+        label.setIcon(image);
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+        frame.setVisible(true);
+        frame.add(label);
+    }
+}
